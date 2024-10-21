3e59ae1205385208b7d37163d15dc3d445bd3848
Kiran Kamble <Kiran.Kamble+EYGS@ey.com>
2024-10-21 17:01:00 +0530

    delete

--- Terraform-resource/Azure KV/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "key_vault_id" {
-  description = "The ID of the Key Vault"
-  value       = azurerm_key_vault.keyvault.id
-}
-
-output "key_vault_uri" {
-  description = "The URI of the Key Vault"
-  value       = azurerm_key_vault.keyvault.vault_uri
-}
